913f6b760a6f7fe9e66dbedb9ba0c48b36cd8578
Kamil Zazula <zazi124@op.pl>
2019-11-21 00:05:34 +0100

    Zadanie 1 - Praca domowa

---
+++ zad_1.1.py
@@ -0,0 +1,20 @@
+# ziemniaki_kg = float(input('Ile kosztuje kilogram ziemniaków? '))
+# waga = 5
+# print(f'Za {waga} kg ziemniaków trzeba zapłacić {ziemniaki_kg * waga} zł.')
+#
+# ziemniaki_kg = float(input('Ile kosztuje kilogram ziemniaków? '))
+# ile_kg = float(input('Ile kilogramów ziemniaków chcesz kupić? '))
+# print(f'Za {ile_kg} kg ziemniaków będziesz musiał zapłacić {ziemniaki_kg * ile_kg} zł')
+
+ziemniaki_cena = float(input('Ile kosztuje kg ziemniaków? '))
+ziemniaki_waga = float(input('Ile kg ziemniaków chcesz kupić? '))
+banany_cena = float(input('Ile kosztuje kg bananów? '))
+banany_waga = float(input('Ile kg bananów chcesz kupić? '))
+
+print(f'Za ziemniaki i banany trzeba zapłacić {ziemniaki_cena * ziemniaki_waga + banany_cena * banany_waga} zł')
+if ziemniaki_cena * ziemniaki_waga > banany_cena * banany_waga:
+    print('Za ziemniaki trzeba zapłacić wiecej.')
+elif ziemniaki_cena * ziemniaki_waga == banany_cena * banany_waga:
+    print('Za ziemniaki i banany trzeba zapłacić taką samą kwotę.')
+else:
+    print('Za banany trzeba zapłacić więcej.')
