847b2cf7599be65f19d1f4ec734a288d2f4e567f
Kamil Zazula <zazi124@op.pl>
2019-11-22 15:23:11 +0100

    Zadanie 1.2 - Praca domowa

---
+++ zad_1.3.py
@@ -0,0 +1,22 @@
+"""
+### Zadanie 1.3 | Współczynnik BMI (ok. 2 godz.)
+​
+Napisz trzy programy, które dla podanych liczb: wzrostu w cm i masy ciała w kg obliczą i wypiszą współczynnik BMI,
+oraz podsumowanie informujące o stanie/zaleceniach. (Informacje o BMI: wzór, interpretację wyników, proszę znaleźć samodzielnie).
+Programy mają różnić się sposobem interakcji z użytkownikiem.
+"""
+
+wzrost = float(input('Podaj swój wzrost w cm: '))
+waga = float(input('Podaj swoją wagę w kg: '))
+
+BMI = waga / (wzrost/100) ** 2
+print(f"Twoje BMI wynosi {BMI}.")
+
+if BMI < 18.5:
+    print("Masz niedowagę.")
+elif 18.5 <= BMI < 25:
+    print("Twoja waga jest prawidłowa.")
+elif 25 <= BMI < 30:
+    print("Masz nadwagę.")
+else:
+    print("Twój wskaźnik BMI wskazuje otyłość.")
