218f1f3e00a3a0c04cd3b9228d02577f87a8d64d
Kamil Zazula <zazi124@op.pl>
2019-11-23 00:37:32 +0100

    Praca domowa

---
+++ zad_1.9.py
@@ -0,0 +1,4 @@
+"""### Zadanie 1.9 FizzBuzz
+​
+Napisz program, który wyświetla liczby od 1 do 100. Dla liczb podzielnych przez 3 niech program wyświetli `Fizz`;
+dla liczb podzielnych przez 5 niech wyświetli `Buzz`; a dla liczb podzielnych przez 15 niech wyświetli `FizzBuzz`."""
