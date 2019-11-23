eae6bd8d559de91edfabae9819ef7d23f0ac983f
Kamil Zazula <zazi124@op.pl>
2019-11-23 14:55:27 +0100

    Praca domowa

---
+++ zad_2.2.py
@@ -0,0 +1,19 @@
+"""### Zadanie 2.2 | Choinka
+​
+Napisz program, który wczytuje liczbę całkowitą, a następnie na konsolę wypisuje w tylu liniach "choinkę" ze znaków `*`. Np. dla parametru 3 powinno się wypisać:
+```
+    *
+  * * *
+* * * * *
+```
+"""
+licznik = int(0)
+x = int(input('Podaj liczbę całkowitą: '))
+gwiazda = '* '
+gwiazdka = '* '
+while licznik != x:
+    print(f'{gwiazdka:^40}', end='')
+    gwiazdka = gwiazdka + 2 * gwiazda
+    licznik += 1
+    print()
+
