85f50105bb38df64e91cc40f4e305d0a614f25e8
Kamil Zazula <zazi124@op.pl>
2019-12-08 16:05:18 +0100

    Praca domowa

--- zad_12_2_kolekcje_30_11_19.py
+++ zad_12_2_kolekcje_30_11_19.py
@@ -2,15 +2,31 @@
 algorytmu "sortowanie przez wybieranie"."""
 
 
-lista = [1500, 500, 2000, 400, 2100, 300]
+# lista = [1500, 500, 2000, 400, 2100, 300]
+# posortowane = []
+#
+# while lista != []:
+#     index_min = lista.index(min(lista))
+#     minimalna = lista.pop(index_min)
+#     posortowane.append(minimalna)
+#
+# print(posortowane)
+
+
+lista = [400, 200, 600, 700, 800, 300]
+wartosc_min = None
 posortowane = []
 
 while lista != []:
-    index_min = lista.index(min(lista))
-    minimalna = lista.pop(index_min)
-    posortowane.append(minimalna)
+    for wartosc in lista:
+        if wartosc_min is None or wartosc < wartosc_min:
+            wartosc_min = wartosc
+    posortowane.append(wartosc_min)
+    lista.remove(wartosc_min)
+    wartosc_min = None
 
 print(posortowane)
+
 
 
 
