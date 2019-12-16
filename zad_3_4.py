fc41d87418f4e83e18881d755864d12bc03a4200
kamilzazi <zazi124@op.pl>
2019-12-16 23:49:52 +0100

    Funkcje

--- zad_3_4.py
+++ zad_3_4.py
@@ -5,28 +5,36 @@ Napisz dekorator `crazy_case`, który co drugą literę w słowie będzie zamien
 ```python
 @crazy_case
 def powiedz_ala():
-	return 'Ala ma kota'
+    return 'Ala ma kota'
 
 print(powiedz_ala()) # aLa mA KoTa
 """
 
-def crazy_case(napis: str) ->str:
+
+def crazy_case(funkcja_do_udekorowania):
     """
     Funkcja pobiera napis od użytkownika i zwraca go ze zmianą co drukiej litery na dużą.
     :param napis:
     :return:
     """
-    nowy_napis = ''
-    for i in range(0, len(napis)):
-        if i % 2 == 0:
-            litera = napis[i]
-            nowy_napis += litera.upper()
-        else:
-            nowy_napis += napis[i]
-    return nowy_napis
+    def wrapper(*args, **kwargs):
+        wynik = funkcja_do_udekorowania(*args, **kwargs)
+        nowy_napis = ''
+        for i in range(0, len(wynik)):
+            if i % 2 == 0:
+                litera = wynik[i]
+                nowy_napis += litera.upper()
+            else:
+                nowy_napis += wynik[i]
+        return nowy_napis
+    return wrapper
 
 
-def jakis_napis():
-    return 'ala ma kota, a kot ma alę'
+@crazy_case
+def jakis_napis(arg = 'kota'):
+    return f'ala ma {arg}, a kot ma alę'
 
-print(crazy_case(jakis_napis()))
+
+print(jakis_napis())
+# print(crazy_case(jakis_napis()))
+
