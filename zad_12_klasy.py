f63ca5c60d8bf251782852ac731ed4b3a157703e
kamilzazi <zazi124@op.pl>
2020-01-04 20:41:14 +0100

    Programowanie obiektowe

--- zad_12_klasy.py
+++ zad_12_klasy.py
@@ -7,56 +7,46 @@ basket = Basket()
 discount = ValueDiscount(10.0)
 basket.add_discount(discount)"""
 
+
 class Product:
-    def __init__(self, id, nazwa, cena):
+    def __init__(self, id, produkt, cena):
         self.id = id
-        self.nazwa = nazwa
+        self.produkt = produkt
         self.cena = cena
 
-    def print_info(self):
-        print(f"{self.nazwa}, ID: {self.id}, Cena: {self.cena} PLN")
-
     def __str__(self):
-        return f"Nazwa: {self.nazwa}, ID: {self.id}, Cena: {self.cena} PLN"
+        return (f"B) ID: {self.id}, produkt: {self.produkt}, cena: {self.cena}")
 
-    def __repr__(self):
-        return f"<Product ID: {self.id}>"
 
 class Basket:
     def __init__(self):
-        self.zawartosc = []
-        self.suma = 0
+        self.koszyk = []
 
-    def add_product(self, p: Product, ilosc: int = 1):
-        for s in self.zawartosc:
-            if s["produkt"].id == p.id:
-                s["ilosc"] += ilosc
+    def add_product(self, produkt: Product, liczba=1):
+        for s in self.koszyk:
+            if s["produkt"].id == produkt.id:
+                s["liczba"] += liczba
                 return
-        # jeśli tu dojdziemy to znaczy, ze na liscie nie ma jeszcze takiego produktu
-        slownik = {"produkt": p, "ilosc": ilosc}
-        self.zawartosc.append(slownik)
-
-
-    def add_discount(self, discount):
-        if self.suma > 0:
-            for slow in self.zawartosc:
-                self.suma += slow["produkt"].cena * slow["ilosc"]
-            return self.suma - discount
-
+        slownik = {"produkt": produkt, "liczba": liczba}
+        self.koszyk.append(slownik)
 
     def count_total_price(self):
-        return self.suma
+        self.total_price = 0
+        for s in self.koszyk:
+            self.total_price += (s["produkt"].cena * s["liczba"])
+        return self.total_price - self.discount
 
-
-class ValueDiscount:
-    def __init__(self, discount):
-        self.discount = discount
-
-    def __float__(self):
+    def add_discount(self, discount1=0):
+        self.discount = discount1
         return self.discount
 
 
+class ValueDiscount:
+    def __init__(self, discount1):
+        self.discount = int(discount1)
 
+    def return_discount(self):
+        return self.discount
 
 
 p1 = Product(1, "Woda", 3)
@@ -67,8 +57,11 @@ x.add_product(p1)
 x.add_product(p2)
 x.add_product(p3)
 x.add_product(p1)
-print(x.zawartosc)
+print(x.koszyk)
 print(x.count_total_price())
-discount = ValueDiscount(10)
-x.add_discount(discount)
-print(x.count_total_price())
+discount = ValueDiscount(10).return_discount()
+z = x.add_discount(discount)
+print("X")
+print(z)
+# print(x.count_total_price())
+# x.add_discount(dis)
